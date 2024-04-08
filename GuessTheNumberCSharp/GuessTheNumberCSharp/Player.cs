d0a41db3798e7029b5bb97721fe86ab26e67aa2a
kittyxcode <katherine.pino059@gmail.com>
2024-04-08 15:32:03 -0400

    ops: update class Player.cs

--- GuessTheNumberCSharp/GuessTheNumberCSharp/Player.cs
+++ GuessTheNumberCSharp/GuessTheNumberCSharp/Player.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 namespace GuessTheNumberCSharp
 {
-    public class Player
+    /*
+    Características de la clase Player:
+    Propiedad que almacena el nombre.
+    Propiedad que almacena el último intento realizado por la jugadora.
+    Constructor que inicializa el último intento en cero y el nombre según el valor con el que fue instanciado.
+    Método que se encarga de hacer la predicción(MakeGuess()) solicitando por terminal un número y validando que sea correcto.*/
+
+    public abstract class Player
+
     {
-        public Player()
+        //campo y propiedad nombre
+        private string name = "";
+        public string Name { get => name; set => name = value; }
+
+        //campo y propiedad que almancena intentos
+        private List<int> listGuess;
+        public List<int> ListGuess { get => listGuess; set => listGuess = value; }
+
+        //constructor
+        protected Player(string name)
         {
+            Name = name;
+            listGuess = new List<int>();
         }
+
+        //metodo abstracto
+        public abstract int MakeGuess();
     }
 }
 
