b8359a43a42ac86a922c414be009d7d22597d4c0
kittyxcode <katherine.pino059@gmail.com>
2024-04-08 15:33:47 -0400

    ops: update class AIPlayer.cs

--- GuessTheNumberCSharp/GuessTheNumberCSharp/AIPlayer.cs
+++ GuessTheNumberCSharp/GuessTheNumberCSharp/AIPlayer.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 namespace GuessTheNumberCSharp
 {
-    public class AIPlayer
+    public class AIPlayer : Player
     {
-        public AIPlayer()
+        //campo y propiedad que permite usar el random
+        private Random random;
+        public Random Random { get => random; set => random = value; }
+
+        //constructor
+        public AIPlayer(string name) : base(name)
         {
+            random = new Random();
+        }
+
+        //metodo sobreescrito
+        public override int MakeGuess()
+        {
+            return random.Next(1, 100);
         }
     }
 }
