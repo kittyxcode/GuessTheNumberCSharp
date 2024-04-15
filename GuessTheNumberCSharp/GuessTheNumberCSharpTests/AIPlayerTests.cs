4677bee534c3bdde3cad0488d0e82b5f10176050
kittyxcode <katherine.pino059@gmail.com>
2024-04-15 13:01:20 -0400

    test: add test for IAPlayer.cs in testProject

--- GuessTheNumberCSharp/GuessTheNumberCSharpTests/AIPlayerTests.cs
+++ GuessTheNumberCSharp/GuessTheNumberCSharpTests/AIPlayerTests.cs
@@ -1,10 +1,21 @@
 ﻿namespace GuessTheNumberCSharpTests;
+using GuessTheNumberCSharp;
 
 [TestClass]
-public class UnitTest1
+public class AIPlayerTests
 {
     [TestMethod]
-    public void TestMethod1()
+    public void MakeGuessTestNumber()
     {
+        // Arrange
+        var random = new Random();
+        GuessTheNumberCSharp.AIPlayer player = new AIPlayer("Computina");
+        // Act
+        int guess = player.MakeGuess();
+
+        // Assert
+        Assert.IsTrue(guess >= 1 && guess <= 100); // Verifica que el número esté en el rango correcto
+        CollectionAssert.Contains(player.ListGuess, guess); // Verifica que el número esté en la lista de intentos
+
     }
 }
